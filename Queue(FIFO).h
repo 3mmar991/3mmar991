c3d31191fcc643dbfc9031f29b92a3bb3e26c7da
3mmar991 <142740814+3mmar991@users.noreply.github.com>
2023-09-18 20:31:38 +0300

    Create Queue(FIFO).h
    Queue function header file

---
+++ Queue(FIFO).h
@@ -0,0 +1,14 @@
+//QUEUE.h
+#define QUEUE_SIZE  5
+#define NULL_P   0
+
+typedef enum
+{
+    QUEUE_empty=0,
+    QUEUE_full,
+    QUEUE_ok
+
+} QUEUE_Status;
+
+QUEUE_Status in_Q(int data);
+QUEUE_Status de_Q(int*num);
